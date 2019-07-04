fce2ffc3fa44a3a48bffb77e22742a42622a79e1
czare <josue@gmx.at>
2019-07-04 16:43:38 -0300

    Inserting User by POST request

--- src/main/java/com/czare/workshopmongo/services/UserService.java
+++ src/main/java/com/czare/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.czare.workshopmongo.domain.User;
+import com.czare.workshopmongo.dto.UserDTO;
 import com.czare.workshopmongo.repository.UserRepository;
 import com.czare.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
