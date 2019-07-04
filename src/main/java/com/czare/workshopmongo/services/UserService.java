e997dd9b490daac6f2a1a54d076a7df6901b51d8
czare <josue@gmx.at>
2019-07-04 16:50:15 -0300

    Deleting a User

--- src/main/java/com/czare/workshopmongo/services/UserService.java
+++ src/main/java/com/czare/workshopmongo/services/UserService.java
@@ -13,23 +13,28 @@ import com.czare.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
